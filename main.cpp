5952dba2fe636e64fcf7f7cf173a031a4b18a918
sora_suzuki <k022g0047@g.neec.ac.jp>
2024-10-01 12:13:35 +0900

    .editorconfigを追加 これで00_01完成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <Windows.h>
 
 int main()
 {
+	//SetConsoleOutputCP(65001);//これで文字化けがしなくなる
+	//char str[] = "ソ";
+
+	//printf("%s", str);
+
 	printf("ちたに　けらは　とほら　すての\nはてき　らとな　りはし　てと");
 
 	return 0;
