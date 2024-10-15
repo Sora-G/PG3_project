427ac8295d96d99d4396eb9b3d421dc6b06dca45
sora_suzuki <k022g0047@g.neec.ac.jp>
2024-10-15 13:15:23 +0900

    コールバック関数の作成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <windows.h>
+
+typedef void (*PFunc)(int*);
+
+//コールバック関数
+void DispResult(int* s) {
+	printf("%d秒待って実行されたよ\n", *s);
+}
+
+void SetTimeOut(PFunc p, int second) {
+	//コールバック関数を呼び出す
+	Sleep(second * 1000);
+
+	p(&second);
+}
 
 int main()
 {
+	printf("strat\n");
+
+	PFunc p;
+	p = DispResult;
+	SetTimeOut(p, 5);
 
 	return 0;
 }
