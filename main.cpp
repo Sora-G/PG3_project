e66c746ec2e1fd2972e182cd08d1b5c6404a71b8
sora_suzuki <k022g0047@g.neec.ac.jp>
2024-10-15 10:36:41 +0900

    再起関数の作成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+int Recursive(int n)
+{
+	if (n <= 1) {
+		return 1;
+	}
+
+	return n * Recursive(n - 1);
+}
+
 int main()
 {
+	int num = 4;
+	int result = 0;
+
+	result = Recursive(num);
+	printf("result [%d]", result);
 
 	return 0;
 }
