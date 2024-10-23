e756d7a470175335fe0ded6c4c848b640f9f0729
sora_suzuki <k022g0047@g.neec.ac.jp>
2024-10-23 15:01:46 +0900

    ラムダ式を使ってみる

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <functional>
 
 int main()
 {
+	//[]() {printf("‚Û‚Û"); }();
+
+	//auto fx = [](int i) {return i + 1; };
+
+	//int num = 10;
+	//auto fx = [=](int i) {return num + i; };
+
+	//printf("%d", fx(2));
+
+	//std::function<int(int)> fx = [](int i) {return i + 1; };
+	//auto fx2 = [](int i) {return i + 1; };
+
+
 
 	return 0;
 }
