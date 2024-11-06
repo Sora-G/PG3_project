b46e163a84eb229b58606bc77baf60361f0bb30e
sora_suzuki <k022g0047@g.neec.ac.jp>
2024-11-06 19:12:41 +0900

    02-04完成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
-#include <stdio.h>
+#include<stdio.h>
+#include"Circle.h"
+#include"Rectangle.h"
+int main() {
+	IShape* iShape[2];
+	iShape[0] = new Circle;
+	iShape[1] = new Rectangle;
 
-int main()
-{
+	for (int i = 0; i < 2; i++) {
+		iShape[i]->Size();
+		iShape[i]->Draw();
+	}
+	for (int i = 0; i < 2; i++) {
+		delete	iShape[i];
+	}
 
 	return 0;
 }
